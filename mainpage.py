83a37ee537cda53cd3d2b67e25940868e98ce35d
Haw Wong <63915576+haw-wong@users.noreply.github.com>
2026-03-30 19:13:35 +0800

    Add files via upload

--- mainpage.py
+++ mainpage.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 def tabMain_arr(tab):
     with tab:
-        st.markdown(f"### :blue[Main page with ship data common to all log modules.]")
+        st.markdown(f"#### :blue[Main page with ship data common to all log modules.]")
+        st.markdown(f"##### :yellow[Data on this page can only be edited by administrator.]")
     return
