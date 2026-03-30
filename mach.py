2b72f7816d9b669aed3c6b3e7b3b8751f39969ff
Haw Wong <63915576+haw-wong@users.noreply.github.com>
2026-03-30 19:19:26 +0800

    Add files via upload

--- mach.py
+++ mach.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def tabMach_arr(tab):
     with tab:
-        col1, col2, col3, col4, col5 = st.columns([0.2,0.3,0.3,0.1,0.1])
+        col1, col2, col3, col4 = st.columns([0.2,0.3,0.25,0.25])
         with col1:
             st.selectbox("Watch",["0000:0400","0400-0800","0800-1200"],index=0)
         with col2:
@@ -13,10 +13,11 @@ def tabMach_arr(tab):
             #                    terminology used in reporting for different sectors/industry/country. We only download financial statements \
             #                    from yahoo finance at this time. Note : When column \'Current\' only contains \'-\', it means that current \
             #                    values have not been successfully downloaded.", horizontal=True)
-        with col4:
+        with col3:
             st.markdown("Section Status: 🔴") #:red_circle:")
-        with col5:
-            st.markdown("Log Book Status: 🟢")
+        with col4:
+            st.markdown("Section Status: 🔴")
+            #st.markdown("Log Book Status: 🟢")
         if selected=="Main Engine":
             dfME = pd.read_excel("erlb.xlsx",sheet_name="mainengine",
                             usecols='X:AP', skiprows=range(23), # Skip rows before the start
