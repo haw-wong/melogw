cd182bf10ccde7628cb14e35c813f761e6e19053
Haw Wong <63915576+haw-wong@users.noreply.github.com>
2026-04-03 15:47:22 +0800

    Add files via upload

--- ship.py
+++ ship.py
@@ -4,6 +4,7 @@ import pandas as pd
 def tabShip_arr(tab):
     with tab:
         st.markdown(f"##### :blue[Ship Information]")
-        dfShip = pd.read_excel('mydata.xlsx', sheet_name="ship")
-        st.dataframe(dfShip, hide_index=True)
+        dfShip = pd.read_excel('erlb.xlsx', sheet_name="ship")
+        dfShip = dfShip.fillna("")
+        st.dataframe(dfShip, hide_index=True, width=500)
     return
