ebd2f18b3363876d90ab3cd0071d14eb5f8457f9
Haw Wong <63915576+haw-wong@users.noreply.github.com>
2026-04-11 10:10:39 +0800

    Delete mainpage.py

--- mainpage.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def tabMain_arr(tab):
-    with tab:
-        st.markdown(f"#### :blue[Main page with ship data common to all log modules.]")
-        st.markdown(f"##### :yellow[Data on this page can only be edited by administrator.]")
-    return
